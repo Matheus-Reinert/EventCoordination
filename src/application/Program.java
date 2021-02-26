f03da63ba3e7fb86b5c57cea8c85ee57d689ad73
Matheus-Reinert <reinert.matheus@hotmail.com>
2021-02-26 14:49:06 -0300

    Adc classes de registro

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,17 @@
 package application;
 
+import model.entities.People;
+
 public class Program {
 
+    public static void main(String[] args) {
+
+        System.out.println(" Java event");
+
+        System.out.println("");
+
+
+
+    }
 
 }
